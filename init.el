f7091bec3c959795ac3e1f56483f76adb1e6b28d
Maxim Kim <habamax@gmail.com>
2023-03-27 20:18:09 +1100

    feat: do not turn on erc-notifications-mode on windows

--- init.el
+++ init.el
@@ -164,7 +164,7 @@
   :ensure nil
   :commands erc
   :config
-  (erc-notifications-mode t)
+  (when (not +IS-WINDOWS+) (erc-notifications-mode t))
   (setq erc-nick '("habamax" "mxmkm")
         erc-track-minor-mode t
         erc-hide-list '("JOIN" "PART" "QUIT")
