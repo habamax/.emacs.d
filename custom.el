c10b392df2d82c2d1ed763b1105aec51038554c6
Maxim Kim <habamax@gmail.com>
2023-03-30 09:13:59 +1100

    feat: add custom.el

---
+++ custom.el
@@ -0,0 +1,22 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elfeed-feeds
+   '(("https://ziglang.org/news/index.xml" zig programming)
+     ("https://www.reddit.com/r/emacs/.rss" reddit emacs)
+     ("https://emacsredux.com/atom.xml" emacs)
+     ("https://www.opennet.ru/opennews/opennews_all_utf.rss" news)
+     ("https://www.linux.org.ru/section-rss.jsp?section=1" news)
+     ("https://news.ycombinator.com/rss" news)
+     ("https://www.youtube.com/feeds/videos.xml?channel_id=UCo4EukJcKyZL6oXoLfsWxfA" youtube game)
+     ("https://www.youtube.com/feeds/videos.xml?channel_id=UCrqM0Ym_NbK1fqeQG2VIohg" youtube programming)))
+ '(package-selected-packages
+   '(zig-mode rust-mode gdscript-mode lorem-ipsum webpaste multiple-cursors elfeed editorconfig erc-hl-nicks markdown-mode magit use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
