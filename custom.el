a9ca148b2a403a9aae6817103013b6f0acbb7a92
Maxim Kim <habamax@gmail.com>
2023-04-22 21:22:44 +1000

    feat: remove newsticker url list

--- custom.el
+++ custom.el
@@ -3,14 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(newsticker-url-list
-   '(("/r/emacs" "https://www.reddit.com/r/emacs/.rss" nil nil nil)
-     ("yt/beastyqt" "https://www.youtube.com/feeds/videos.xml?channel_id=UCo4EukJcKyZL6oXoLfsWxfA" nil nil nil)
-     ("yt/tsoding" "https://www.youtube.com/feeds/videos.xml?channel_id=UCrqM0Ym_NbK1fqeQG2VIohg" nil nil nil)
-     ("Planet Emacs" "https://planet.emacslife.com/atom.xml" nil nil nil)
-     ("linux.org.ru" "https://www.linux.org.ru/section-rss.jsp?section=1" nil nil nil)
-     ("opennet.ru" "https://www.opennet.ru/opennews/opennews_all_utf.rss" nil nil nil)))
- '(newsticker-url-list-defaults nil)
  '(package-selected-packages
    '(marginalia rainbow-mode htmlize zig-mode gdscript-mode lorem-ipsum webpaste editorconfig erc-hl-nicks markdown-mode magit use-package))
  '(warning-suppress-types '((org) (org))))
