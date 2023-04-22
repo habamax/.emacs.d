276b9c9b220c5f5f8a00a348588e570f97d62ecb
Maxim Kim <habamax@gmail.com>
2023-04-22 16:32:09 +1000

    feat: remove elfeed

--- custom.el
+++ custom.el
@@ -3,17 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(elfeed-feeds
-   '(("https://ziglang.org/news/index.xml" zig programming)
-     ("https://www.reddit.com/r/emacs/.rss" reddit emacs)
-     ("https://emacsredux.com/atom.xml" emacs)
-     ("https://www.opennet.ru/opennews/opennews_all_utf.rss" news)
-     ("https://www.linux.org.ru/section-rss.jsp?section=1" news)
-     ("https://news.ycombinator.com/rss" news)
-     ("https://www.youtube.com/feeds/videos.xml?channel_id=UCo4EukJcKyZL6oXoLfsWxfA" youtube game)
-     ("https://www.youtube.com/feeds/videos.xml?channel_id=UCrqM0Ym_NbK1fqeQG2VIohg" youtube programming)))
  '(package-selected-packages
-   '(rainbow-mode htmlize zig-mode gdscript-mode lorem-ipsum webpaste elfeed editorconfig erc-hl-nicks markdown-mode magit use-package))
+   '(rainbow-mode htmlize zig-mode gdscript-mode lorem-ipsum webpaste editorconfig erc-hl-nicks markdown-mode magit use-package))
  '(warning-suppress-types '((org) (org))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
