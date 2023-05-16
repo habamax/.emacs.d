a56e1c0cf5df3354c18afe4a25c8d6bb3ca13191
Maxim Kim <habamax@gmail.com>
2023-05-16 21:02:27 +1000

    corfu: default expansion

--- init.el
+++ init.el
@@ -347,14 +347,6 @@
     (xclip-mode 1)))
 
 (use-package corfu
-  ;; :custom
-  ;; (corfu-cycle t)
-  ;; ;; (corfu-preselect 'prompt)
-  ;; :bind (:map corfu-map
-  ;;             ("TAB" . corfu-next)
-  ;;             ([tab] . corfu-next)
-  ;;             ("S-TAB" . corfu-previous)
-  ;;             ([backtab] . corfu-previous))
   :init
   (global-corfu-mode)
   :config
