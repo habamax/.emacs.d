e736bca4ebda4715c9b5ad020fae1619ef58b6ac
Maxim Kim <habamax@gmail.com>
2023-05-21 22:49:28 +1000

    fix notmuch delete

--- lisp/habamax-notmuch.el
+++ lisp/habamax-notmuch.el
@@ -22,12 +22,12 @@
 
 (defun notmuch-sync ()
   (interactive)
-  (notmuch-delete)
   (when (executable-find "mbsync")
+    (notmuch-delete)
     (compile "mbsync -a && notmuch new")))
 
 (defun notmuch-delete ()
-  "It doesn't delete in gmail... Just put emails from Inbox to All..."
+  "Delete emails marked with `deleted' tag."
   (interactive)
   (let* ((del-tag "deleted")
          (count
@@ -37,8 +37,8 @@
               (format "notmuch count tag:%s" del-tag) t)
              (buffer-substring-no-properties (point-min) (1- (point-max))))))
          (mail (if (> count 1) "mails" "mail")))
-    (unless (> count 0)
-      (format "Deleting %d %s marked as `%s'..." count mail del-tag)
+    (when (> count 0)
+      (message "Deleting %d %s marked as `%s'..." count mail del-tag)
       (shell-command
        (format "notmuch search --output=files --format=text0 tag:%s | %s"
                del-tag "xargs -r0 rm")))))
