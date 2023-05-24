efdca98f9bdb0046dd5981503092ec81aedcadae
Maxim Kim <habamax@gmail.com>
2023-05-24 18:00:59 +1000

    Increase iosevka font size in windows

--- init.el
+++ init.el
@@ -23,7 +23,7 @@
       smtpmail-smtp-service 587)
 
 (if +IS-WINDOWS+
-    (let ((fonts '(("Iosevka Term SS04" . "14")
+    (let ((fonts '(("Iosevka Term SS04" . "17")
                    ("JetBrains Mono NL" . "14")
                    ("Dejavu Sans Mono"  . "14")
                    ("Consolas"          . "14"))))
