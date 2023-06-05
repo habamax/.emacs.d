5454964679cd6992cf241db661b8f62d40170d3e
Maxim Kim <habamax@gmail.com>
2023-06-05 11:43:36 +1000

    format setq

--- init.el
+++ init.el
@@ -102,9 +102,8 @@
   (package-install 'use-package))
 
 (require 'use-package)
-(setq
- use-package-always-ensure t
- use-package-always-defer t)
+(setq use-package-always-ensure t
+      use-package-always-defer t)
 
 (use-package habamax
   :load-path "lisp"
