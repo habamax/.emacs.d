06f3570f42731624e2bda3349e84c0105757cc8c
Maxim Kim <habamax@gmail.com>
2023-06-05 11:33:39 +1000

    join #commonlisp

--- lisp/habamax-erc.el
+++ lisp/habamax-erc.el
@@ -8,7 +8,7 @@
       erc-hide-list '("JOIN" "PART" "QUIT")
       erc-join-buffer 'bury
       erc-autojoin-channels-alist '(("Libera.Chat"
-                                     "#emacs" "#vim" "#python" "#zig"))
+                                     "#emacs" "#vim" "#python" "#zig" "#commonlisp"))
       erc-server-reconnect-attempts 5
       erc-server-reconnect-timeout 3)
 
