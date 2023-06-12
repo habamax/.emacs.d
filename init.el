8aab442f4e2fb269a49137d8f81b6be8a10d5d0e
Maxim Kim <habamax@gmail.com>
2023-06-12 22:09:30 +1000

    add consult-org-agenda bind

--- init.el
+++ init.el
@@ -208,6 +208,7 @@
          ("M-s s" . consult-line)
          ("M-s M-s" . consult-line)
          ("M-g o" . consult-outline)
+         ("C-c o a" . consult-org-agenda)
          ([remap goto-line] . consult-goto-line)
          ("M-s g" . consult-grep)
          ("M-s r" . consult-ripgrep)
