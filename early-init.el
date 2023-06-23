6152ddd4d473d039e65bbbe618d31a484436c743
Maxim Kim <habamax@gmail.com>
2023-06-23 23:22:16 +1000

    back to 95 alpha

--- early-init.el
+++ early-init.el
@@ -23,7 +23,7 @@
 (setq-default
  default-frame-alist
  '((font . "Monospace-18")
-   (alpha-background . 92)
+   (alpha-background . 95)
    (fullscreen . maximized)
    (horizontal-scroll-bars . nil)
    (vertical-scroll-bars . nil)
