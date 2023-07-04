db212b23621f844f1392c8645a98111198a3951c
Maxim Kim <habamax@gmail.com>
2023-07-04 13:10:44 +1000

    wildcharm: Add license, update comments

--- themes/wildcharm-theme.el
+++ themes/wildcharm-theme.el
@@ -1,12 +1,27 @@
 ;;; wildcharm-theme.el --- port of vim-wildcharm colorscheme -*- lexical-binding: t; -*-
+
 ;; Author: Maxim Kim <habamax@gmail.com>
+;; URL: https://github.com/habamax/wildcharm-theme
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
-;; Dark theme with contrast colors.
-;; TUI has no background defined, i.e. depends on terminal.
-;; Should look mostly the same in GUI and TUI with 256 colors support.
-;; NOTE: it is not exactly the same as in vim.
+;; Contrast dark emacs theme.
+;; - Port of the dark background vim-wildcharm colorscheme.
+;; - TUI has no background defined, i.e. depends on terminal.
+;; - Should look mostly the same in GUI and TUI with 256 colors support.
 
 ;;; Code:
 
