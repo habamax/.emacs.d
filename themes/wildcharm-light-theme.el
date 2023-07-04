89cb44b22aefb0e75c4e192db1c2207eb25a75fb
Maxim Kim <habamax@gmail.com>
2023-07-04 15:23:17 +1000

    wildcharm: follow M-x checkdoc

--- themes/wildcharm-light-theme.el
+++ themes/wildcharm-light-theme.el
@@ -18,7 +18,7 @@
 
 ;;; Commentary:
 
-;; Contrast light emacs theme.
+;; Contrast light Emacs theme.
 ;; - Port of the light background vim-wildcharm colorscheme.
 ;; - Should look mostly the same in GUI and TUI with 256 colors support.
 
--- themes/wildcharm-theme.el
+++ themes/wildcharm-theme.el
@@ -18,7 +18,7 @@
 
 ;;; Commentary:
 
-;; Contrast dark emacs theme.
+;; Contrast dark Emacs theme.
 ;; - Port of the dark background vim-wildcharm colorscheme.
 ;; - TUI has no background defined, i.e. depends on terminal.
 ;; - Should look mostly the same in GUI and TUI with 256 colors support.
