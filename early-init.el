a0bb0117caf29c2f33aad325ec4a99cdb5ff3c2a
Maxim Kim <habamax@gmail.com>
2023-07-24 13:31:34 +1000

    remove transparency by default

--- early-init.el
+++ early-init.el
@@ -19,7 +19,6 @@
 (setq-default
  default-frame-alist
  '((font . "Monospace-18")
-   (alpha-background . 90)
    (fullscreen . maximized)
    (horizontal-scroll-bars . nil)
    (vertical-scroll-bars . nil)
