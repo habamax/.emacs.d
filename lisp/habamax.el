c9bd932c491ae0d76685869fc9decd0c06b6d259
Maxim Kim <habamax@gmail.com>
2023-11-10 00:13:11 +1100

    fix toggle-theme

--- lisp/habamax.el
+++ lisp/habamax.el
@@ -102,7 +102,7 @@ Otherwise call well known `comment-dwim'"
   (let* ((theme (car custom-enabled-themes))
          (my-themes '(nocharm-p nocharm-light wildcharm nocharm-white))
          (next-theme (seq-elt my-themes
-                              (1+ (seq-position my-themes theme)))))
+                              (1+ (or (seq-position my-themes theme) -1)))))
     (mapc #'disable-theme custom-enabled-themes)
     (ignore-errors (load-theme (or next-theme (car my-themes)) t))))
 
