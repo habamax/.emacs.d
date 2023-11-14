eaab35a6cde57d3ad86395491e1d17f8395a6de7
Maxim Kim <habamax@gmail.com>
2023-11-15 10:13:43 +1100

    fido mode

--- init.el
+++ init.el
@@ -209,6 +209,8 @@
   (diminish 'eldoc-mode))
 
 (use-package icomplete
+  :init
+  (fido-mode)
   :config
   (setq icomplete-compute-delay 0))
 
@@ -261,13 +263,13 @@
   (setq ediff-split-window-function 'split-window-horizontally)
   (setq ediff-window-setup-function 'ediff-setup-windows-plain))
 
-(use-package vertico
-  :bind
-  (:map vertico-map
-        ("DEL" . vertico-directory-delete-char)
-        ("M-DEL" . vertico-directory-delete-word))
-  :init
-  (vertico-mode))
+;; (use-package vertico
+;;   :bind
+;;   (:map vertico-map
+;;         ("DEL" . vertico-directory-delete-char)
+;;         ("M-DEL" . vertico-directory-delete-word))
+;;   :init
+;;   (vertico-mode))
 
 (use-package magit
   :bind (("C-x g" . magit-file-dispatch)
