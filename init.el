7c74aca66f603318385a0cc61074bf8c5389b03c
Maxim Kim <habamax@gmail.com>
2023-11-18 17:33:30 +1100

    increase back tab-bar-tab width

--- init.el
+++ init.el
@@ -38,6 +38,7 @@
 (setq delete-pair-blink-delay 0)
 
 (setq tab-bar-show 1
+      tab-bar-auto-width-max '(360 30)
       tab-bar-close-button-show nil
       tab-bar-format '(tab-bar-format-history
                        tab-bar-format-tabs
