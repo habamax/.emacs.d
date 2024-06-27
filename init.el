4675e708a7b1b04c962849e9e1c1d5035c72201d
Maxim Kim <habamax@gmail.com>
2024-06-27 21:53:33 +1000

    remove evil mode

--- init.el
+++ init.el
@@ -160,14 +160,6 @@
   (outline-7 ((t (:height 1.0 :weight bold))))
   (outline-8 ((t (:height 1.0 :weight bold)))))
 
-(use-package evil
-  :bind
-  (:map
-   minibuffer-mode-map
-   ("<escape>" . minibuffer-keyboard-quit))
-  :init
-  (evil-mode))
-
 (use-package icomplete
   :init
   (fido-mode)
